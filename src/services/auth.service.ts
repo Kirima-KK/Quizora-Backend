369888bdb84f4a6953dd8e4897393953a573f2fd
Kirima Kangwanwongsakul <61814979+Kirima-KK@users.noreply.github.com>
2026-03-25 16:13:26 +0700

    remove unnecessary log

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -50,11 +50,9 @@ class AuthService {
   login = async (currentUser: LoginInfo) => {
     // Check for user existence
     const user = await User.findOne({ email: currentUser.email });
-    console.log(`Login Service-user: ${user} ${currentUser.email}`);
     if (!user) throw new InvalidCredentialError("Invalid Credentials.", 401);
     
     const result = await bcrypt.compare(currentUser.password, user.password);
-    console.log(`Login Service-result: ${result}`);
     if (!result) {
       throw new InvalidCredentialError("Invalid Credentials.", 401);
     }
